e19cc6db169ca11cf9819c67d7583b2412cc825d
이경용 <sleekydz86@icloud.com>
2023-12-27 20:52:46 +0900

    식후땡 프로젝트 시작
    식후땡 프로젝트 시작

---
+++ src/main/java/com/multibook/bookorder/BookorderApplication.java
@@ -0,0 +1,13 @@
+package com.multibook.bookorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multibook/bookorder/BookorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multibook.bookorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
